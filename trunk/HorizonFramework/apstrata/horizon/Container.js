8f1b67485ff3d5622c208919d87bc21db7289f96
rabih.nassar@elementn.com <rabih.nassar@elementn.com@b2800edd-b7cf-27b7-5226-d102153f4265>
2012-04-13 16:59:16 +0000

    added LoginWidget

--- trunk/HorizonFramework/apstrata/horizon/Container.js
+++ trunk/HorizonFramework/apstrata/horizon/Container.js
@@ -35,13 +35,20 @@ dojo.declare("apstrata.horizon.Container",
 	// Provides a unique key for storing preferences data (by default to a cookie)
 	applicationId: "horizon",
 
-	margin: null,
 	_marginRight: 0,
 	controlToolbarClass: apstrata.horizon.ControlToolbar,
 	showToolbar: true,
+
+	//
+	// Widget attributes
+	//
+	margin: null,
+	loginWidget: null,
 	
 	constructor: function(attrs) {
 		if (attrs && attrs.applicationId) this.applicationId = attrs.applicationId
+		
+		this.loginWidget = attrs.loginWidget
 
 		// Extract the request params into an object		
 		var s = window.location.search.substring(1)
@@ -104,17 +111,55 @@ dojo.declare("apstrata.horizon.Container",
 		console.debug('URL Hash changed to: ' + hash)
 	},
 	
+	listenToHashChange: function(v) {
+		if (v) {
+			this._listenUrlHashChange = setInterval(dojo.hitch(this,"_checkUrlHashChange"), 500)
+		} else {
+			clearInterval(this._listenUrlHashChange)
+		}
+	},
+	
+	/**
+	 * Overriden by child classes to login application
+	 * 
+	 * @param {Object} credentials
+	 */
+	onCredentials: function(credentials) {
+		
+	},
+	
 	startup: function() {
+		var self = this
 		//setTimeout(dojo.hitch(this, 'loadPreferences'), 3000)
 		//setTimeout(dojo.hitch(this, 'layout'), 100)
-
-		setInterval(dojo.hitch(this,"_checkUrlHashChange"), 500)
+		
+		if (this.loginWidget) {
+			this.loginWidget.set("dimension", this._boundingRectangle)
+			dojo.place(this.loginWidget.domNode, dojo.body())
+			this.loginWidget.then(function(credentials) {
+				self.onCredentials(credentials)
+				self._instantiateMainPanel()
+				self.listenToHashChange(true)
+				self.loginWidget.destroyRecursive()
+			},
+			function() {
+				
+			})
+		} else {
+			self._instantiateMainPanel()
+			this.listenToHashChange(true)
+		}
 
 		this.inherited(arguments)
 	},
 	
 	autoScroll: function() {
-		this.domNode.scrollLeft = this.domNode.scrollWidth - this.domNode.clientWidth
+		try {
+			this.domNode.scrollLeft = this.domNode.scrollWidth - this.domNode.clientWidth		
+		} catch (e) {
+//			console.error(e)
+		}
+		
 	},
 		
 	getRemainingFreeWidth: function(excludeId) {
@@ -123,20 +168,19 @@ dojo.declare("apstrata.horizon.Container",
 		for (id in this.__children) {
 			if (id != excludeId) {
 				var child = this.__children[id]
-				
-				w += child.domNode.offsetWidth
+				w += dojo.marginBox(child.domNode).w
 			}
 		}
-
-		return this.domNode.offsetWidth - w
+		
+		return dojo.marginBox(this.domNode).w - w
 	},
 	
 	layout: function() {
 		this._containerLayout()
-		
+		if (this.loginWidget) this.loginWidget.set("dimension", this._boundingRectangle)
 		// Call layout for each contained widget upon resize
 		dojo.forEach(this.getChildren(), function(child) {
-			child.resize();
+				child.resize()	
 		})
 
 		if (this._mainPanel) {
@@ -188,7 +232,6 @@ dojo.declare("apstrata.horizon.Container",
 			bounding.width = this._CSSdim.w
 		}
 		
-		
 		var container = {}
 		var fixedPanel = {}
 		
@@ -219,6 +262,9 @@ dojo.declare("apstrata.horizon.Container",
 		background.left = (bounding.left - bMargin)
 		background.width = (bounding.width)
 		background.height = (bounding.height)
+
+		this._boundingRectangle = background
+
 
 		// Calculate Control toolbar position
 		var toolbar = {}
